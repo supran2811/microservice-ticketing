75051427d07c3efb4794474c8e00eb78b35af127
Supran Sengupta <supransengupta@gmail.com>
2020-07-21 07:40:22 +0530

    updated for test

---
+++ auth/src/errors/not-authorised-error.ts
@@ -0,0 +1,19 @@
+import { CustomError } from "./custom-error";
+
+export class NotAuthorisedError extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super("Not authorised");
+
+    Object.setPrototypeOf(this, NotAuthorisedError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: "Not authorised",
+      },
+    ];
+  }
+}
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,28 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "assffff";
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let coll of collections) {
+    await coll.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
