9c2c53209f1b9bb5389708204083c8fec2545a1e
Supran Sengupta <supransengupta@gmail.com>
2020-08-06 06:08:07 +0530

    updates

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -38,7 +38,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload. { id , email }
   const payload = {
-    id: "adasdlskad",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
