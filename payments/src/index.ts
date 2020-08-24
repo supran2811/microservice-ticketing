79c40c6290624f319e1826cb18976f4c005e3f4a
supran2811 <supransengupta@gmail.com>
2020-08-24 17:07:41 +0530

    update

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { natsWrapper } from './nats-wrapper';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 const start = async () => {
+  console.log('Starting....!!!!');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KET must be defined');
   }
