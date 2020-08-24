ca670c5cdf874384ac892b26ef0274c9d236f021
supran2811 <supransengupta@gmail.com>
2020-08-24 17:29:02 +0530

    update

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,21 +1,25 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default ({ req }) => {
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     /// On the server
     /**
      * URL format will be
      * http://<SERVICENAME>.<NAMESPACE>.svc.cluster.local/
      */
+    // return axios.create({
+    //   baseURL:
+    //     "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+    //   headers: req.headers,
+    // });
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: 'http://www.ticketing.supsengu.de',
       headers: req.headers,
     });
   } else {
     /// On the browser
     return axios.create({
-      baseURL: "/",
+      baseURL: '/',
     });
   }
 };
