79d6201893bbcf63d9e43ee9e9824db3d2d1d895
supran2811 <supransengupta@gmail.com>
2020-08-24 17:36:27 +0530

    update

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -13,7 +13,7 @@ export default ({ req }) => {
     //   headers: req.headers,
     // });
     return axios.create({
-      baseURL: 'http://ticketing.supsengu.de',
+      baseURL: 'http://www.ticketing.supsengu.de',
       headers: req.headers,
     });
   } else {
